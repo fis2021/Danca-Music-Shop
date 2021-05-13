436ca6c1e12d19f3337f89962d969d062ef14d0c
eusuntdan <62446352+eusuntdan@users.noreply.github.com>
2021-05-13 23:39:26 +0300

    DMS-51 Show a status message

--- src/application/PrintAlbums.java
+++ src/application/PrintAlbums.java
@@ -100,7 +100,7 @@ public static void printAlbums(GridPane gridPane) {
             	i++;
             	id=String.valueOf(id1);
 	
-			
+		// Show a status message	
 			buyButton.setOnAction(new EventHandler<ActionEvent>() {
 	            @Override
 	            public void handle(ActionEvent event) {                          
